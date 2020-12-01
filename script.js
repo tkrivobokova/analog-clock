428eaf5370220e5807f81b825f6f012c52a22b99
tkrivobokova <tkrivobokova@gmail.com>
2020-12-01 20:56:41 +0200

    created static clock hands

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+let date = new Date();
+
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+
+let minFull = minute * 360 / 60;
+let secPosition = second * 360 / 60;
+let minPosition = minFull + (secPosition / 60);
+let hrPosition = hour * 360 / 60 + (minFull / 12);
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
