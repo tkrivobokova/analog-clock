2cd2b40e7b177d1c53f0c8f46c2baf35c562715b
tkrivobokova <tkrivobokova@gmail.com>
2020-12-01 21:13:42 +0200

    changed variables names

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const SECONDHAND = document.querySelector("#second");
 function runClock() {
 let date = new Date();
 
-let hour = date.getHours();
-let minute = date.getMinutes();
-let second = date.getSeconds();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
 
-let minFull = minute * 360 / 60;
-let secPosition = second * 360 / 60;
-let minPosition = minFull + (secPosition / 60);
-let hrPosition = hour * 360 / 12 + (minFull / 12);
+let minFullPosition = minutes * 360 / 60;
+let secPosition = seconds * 360 / 60;
+let minPosition = minFullPosition + (secPosition / 60);
+let hrPosition = hours * 360 / 12 + (minFullPosition / 12);
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
-const interval = setInterval(runClock, 1000);
+const INTERVAL = setInterval(runClock, 1000);
