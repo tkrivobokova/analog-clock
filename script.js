4223db4df799b354cd66f1ca19459e96e7c368ad
tkrivobokova <tkrivobokova@gmail.com>
2020-12-01 21:01:57 +0200

    fixed typo in calculating the hour position

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
+function runClock() {
 let date = new Date();
 
 let hour = date.getHours();
@@ -12,8 +12,11 @@ let second = date.getSeconds();
 let minFull = minute * 360 / 60;
 let secPosition = second * 360 / 60;
 let minPosition = minFull + (secPosition / 60);
-let hrPosition = hour * 360 / 60 + (minFull / 12);
+let hrPosition = hour * 360 / 12 + (minFull / 12);
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+//const interval = setInterval(runClock, 1000);
